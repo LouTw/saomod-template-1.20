0aafe96578742f32ce6e50de2d1df41a7570c498
LouTw <hzxsloutw@outlook.com>
2024-10-14 23:31:45 +0800

    2024.10.14_4

--- src/main/java/com/lou/sao/world/gen/ModStructure/ModStructurePools.java
+++ src/main/java/com/lou/sao/world/gen/ModStructure/ModStructurePools.java
@@ -1,13 +1,10 @@
 package com.lou.sao.world.gen.ModStructure;
 
-import com.google.common.collect.ImmutableList;
 import com.lou.sao.structures.InitialTownGenerator;
 
 import net.minecraft.registry.Registerable;
-import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.structure.pool.StructurePool;
 import net.minecraft.util.Identifier;
 
@@ -23,9 +20,6 @@ public class ModStructurePools {
 	}
 
 	public static void bootstrap(Registerable<StructurePool> structurePoolsRegisterable) {
-		RegistryEntryLookup<StructurePool> registryEntryLookup = structurePoolsRegisterable.getRegistryLookup(RegistryKeys.TEMPLATE_POOL);
-		RegistryEntry<StructurePool> registryEntry = registryEntryLookup.getOrThrow(EMPTY);
-		structurePoolsRegisterable.register(EMPTY, new StructurePool(registryEntry, ImmutableList.of(), StructurePool.Projection.RIGID));
 
 		InitialTownGenerator.bootstrap(structurePoolsRegisterable);
 	}
--- src/main/java/com/lou/sao/world/gen/ModStructure/ModStructureSets.java
+++ src/main/java/com/lou/sao/world/gen/ModStructure/ModStructureSets.java
@@ -1,36 +1,18 @@
 package com.lou.sao.world.gen.ModStructure;
 
-import java.util.List;
 
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.structure.StructureSet;
-import net.minecraft.structure.StructureSetKeys;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.chunk.placement.RandomSpreadStructurePlacement;
 import net.minecraft.world.gen.chunk.placement.SpreadType;
 import net.minecraft.world.gen.structure.Structure;
-import net.minecraft.world.gen.structure.StructureKeys;
 
 public interface ModStructureSets {
     static void bootstrap(Registerable<StructureSet> structureSetRegisterable) {
 		RegistryEntryLookup<Structure> registryEntryLookup = structureSetRegisterable.getRegistryLookup(RegistryKeys.STRUCTURE);
-		RegistryEntryLookup<Biome> registryEntryLookup2 = structureSetRegisterable.getRegistryLookup(RegistryKeys.BIOME);
-		RegistryEntry.Reference<StructureSet> reference = structureSetRegisterable.register(
-			StructureSetKeys.VILLAGES,
-			new StructureSet(
-				List.of(
-					StructureSet.createEntry(registryEntryLookup.getOrThrow(StructureKeys.VILLAGE_PLAINS)),
-					StructureSet.createEntry(registryEntryLookup.getOrThrow(StructureKeys.VILLAGE_DESERT)),
-					StructureSet.createEntry(registryEntryLookup.getOrThrow(StructureKeys.VILLAGE_SAVANNA)),
-					StructureSet.createEntry(registryEntryLookup.getOrThrow(StructureKeys.VILLAGE_SNOWY)),
-					StructureSet.createEntry(registryEntryLookup.getOrThrow(StructureKeys.VILLAGE_TAIGA))
-				),
-				new RandomSpreadStructurePlacement(34, 8, SpreadType.LINEAR, 10387312)
-			)
-		);
+		
 		structureSetRegisterable.register(
 			ModStructureSetKeys.INITIAL_TOWN_STRUCTURES,
 			new StructureSet(registryEntryLookup.getOrThrow(ModStructureKeys.INITIAL_TOWN_STRUCTURE), new RandomSpreadStructurePlacement(24, 8, SpreadType.LINEAR, 20083232))
