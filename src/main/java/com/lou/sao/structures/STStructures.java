e187d7395fe624dcddb7e6d72a9b124ebaa60d25
LouTw <hzxsloutw@outlook.com>
2024-10-14 11:23:04 +0800

    2024.10.14_2

---
+++ src/main/java/com/lou/sao/structures/STStructures.java
@@ -0,0 +1,23 @@
+package com.lou.sao.structures;
+
+import com.lou.sao.SAOMod;
+
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.structure.StructureType;
+
+public class STStructures {
+     public static StructureType<SkyStructures> SKY_STRUCTURES;
+
+    /**
+     * Registers the structure itself and sets what its path is. In this case, the
+     * structure will have the Identifier of structure_tutorial:sky_structures.
+     *
+     * It is always a good idea to register your Structures so that other mods and datapacks can
+     * use them too directly from the registries. It's great for mod/datapacks compatibility.
+     */
+    public static void registerStructureFeatures() {
+        SKY_STRUCTURES = Registry.register(Registries.STRUCTURE_TYPE, new Identifier(SAOMod.MOD_ID, "sky_structures"), () -> SkyStructures.CODEC);
+    }
+}
